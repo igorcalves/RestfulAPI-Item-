c5ddd071d303c26715cbed4b7c23a26f20aca5a0
igor alves <112185065+igorcalves@users.noreply.github.com>
2023-02-11 09:37:12 -0300

    Delete Spring-Resful-Itens directory

--- Spring-Resful-Itens/src/main/resources/db/migration/V1__Create_Table_Itens.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `itens` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `since` datetime(6) NOT NULL,
-  `price` decimal(65,2) NOT NULL,
-  PRIMARY KEY (`id`)
-);
